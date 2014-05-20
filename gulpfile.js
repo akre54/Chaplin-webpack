939eeb22248c97f31619c70328ae2b6157657d4b
Adam Krebs <amk528@cs.nyu.edu>
2014-05-19 23:57:57 -0400

    no need for css style with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,9 @@ var gulp = require('gulp'),
     webpack = require('webpack'),
     webpackConfig = require('./webpack.config');
 
-gulp.task('default', ['css', 'webpack'], function() {
+gulp.task('default', ['webpack'], function() {
   gulp.src('app/index.html')
       .pipe(gulp.dest('dist'));
-});
-
-gulp.task('css', function() {
-  gulp.src('app/assets/css/*.css')
-      .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('webpack', function (cb) {
